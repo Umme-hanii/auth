f426be5fb7839440b2aa7260740aeb3cdf767739
Umme-hanii <ummehanimohammed9@gmail.com>
2023-05-01 18:44:08 +0530

    Code Refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
-import { InputComponent } from './input/input.component';
-// import { AngularFireModule } from '@angular/fire';
-// import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire/compat'
-// import { provideAuth,getAuth } from '@angular/fire/auth';
-// import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth'
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
@@ -29,9 +25,6 @@ import { RegisterComponent } from './register/register.component';
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    // provideFirebaseApp(() => initializeApp(environment.firebase)),
-    // provideAuth(() => getAuth()),
-    // provideDatabase(() => getDatabase()),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
   ],
